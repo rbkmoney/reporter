020ecc26fb4b1f16f17c967ef99992b048dfb1e4
Pavel Popov <popoffpavel@yandex.ru>
2017-10-27 18:08:43 +0300

    Add more tests

--- src/test/java/com/rbkmoney/reporter/AbstractIntegrationTest.java
+++ src/test/java/com/rbkmoney/reporter/AbstractIntegrationTest.java
@@ -1,17 +1,9 @@
 package com.rbkmoney.reporter;
 
-import com.rbkmoney.reporter.model.PartyModel;
-import com.rbkmoney.reporter.model.ShopAccountingModel;
-import com.rbkmoney.reporter.service.PartyService;
-import com.rbkmoney.reporter.service.SignService;
-import com.rbkmoney.reporter.service.StatisticService;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
-import org.mockito.stubbing.Answer;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.util.EnvironmentTestUtils;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -22,16 +14,6 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.LogMessageWaitStrategy;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Base64;
-
-import static io.github.benas.randombeans.api.EnhancedRandom.random;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
@@ -78,26 +60,6 @@ public class AbstractIntegrationTest {
                     "storage.secretKey=" + AWS_SECRET_KEY
             );
         }
-    }
-
-    @MockBean
-    private StatisticService statisticService;
-
-    @MockBean
-    private PartyService partyService;
-
-    @MockBean
-    private SignService signService;
-
-    @Before
-    public void setup() {
-        given(statisticService.getShopAccounting(anyString(), anyString(), any(Instant.class), any(Instant.class))).willReturn(random(ShopAccountingModel.class));
-        given(statisticService.getPayments(anyString(), anyString(), any(), any())).willReturn(new ArrayList<>());
-        given(partyService.getPartyRepresentation(anyString(), anyString(), any(Instant.class))).willReturn(random(PartyModel.class));
-        given(signService.sign(any(Path.class)))
-                .willAnswer(
-                        (Answer<byte[]>) invocation -> Base64.getEncoder().encode(Files.readAllBytes(invocation.getArgumentAt(0, Path.class)))
-                );
     }
 
     @LocalServerPort
