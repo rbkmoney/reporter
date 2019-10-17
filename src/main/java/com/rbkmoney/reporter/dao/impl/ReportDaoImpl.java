24802f2386f5a91f7f930c26ce86bbc6fdae4776
d.baykov <d.baykov@rbkmoney.com>
2019-10-17 17:07:54 +0300

    Fix obtaining nullable shop ID [2]

--- src/main/java/com/rbkmoney/reporter/dao/impl/ReportDaoImpl.java
+++ src/main/java/com/rbkmoney/reporter/dao/impl/ReportDaoImpl.java
@@ -39,28 +39,9 @@ public class ReportDaoImpl extends AbstractGenericDao implements ReportDao {
     }
 
     @Override
-    public Report getReport(String partyId, String shopId, long reportId) throws DaoException {
-        Query query = getDslContext().selectFrom(REPORT).where(
-                REPORT.ID.eq(reportId)
-                        .and(REPORT.PARTY_ID.eq(partyId))
-                        .and(ofNullable(shopId).map(REPORT.PARTY_SHOP_ID::eq).orElse(trueCondition()))
-        );
-        return fetchOne(query, reportRowMapper);
-    }
-
-    @Override
-    public Report getReportDoUpdate(String partyId, String shopId, long reportId) throws DaoException {
-        Query query = getDslContext().selectFrom(REPORT).where(
-                REPORT.ID.eq(reportId)
-                        .and(REPORT.PARTY_ID.eq(partyId))
-                        .and(ofNullable(shopId).map(REPORT.PARTY_SHOP_ID::eq).orElse(trueCondition())))
-                .forUpdate();
-        return fetchOne(query, reportRowMapper);
-    }
-
-    @Override
     public Report getReport(long reportId) throws DaoException {
-        Query query = getDslContext().selectFrom(REPORT).where(REPORT.ID.eq(reportId));
+        Query query = getDslContext().selectFrom(REPORT).where(
+                REPORT.ID.eq(reportId));
         return fetchOne(query, reportRowMapper);
     }
 
@@ -72,11 +53,9 @@ public class ReportDaoImpl extends AbstractGenericDao implements ReportDao {
     }
 
     @Override
-    public Report getReportDoUpdateSkipLocked(String partyId, String shopId, long reportId) throws DaoException {
-        Query query = getDslContext().selectFrom(REPORT).where(
-                REPORT.ID.eq(reportId)
-                        .and(REPORT.PARTY_ID.eq(partyId))
-                        .and(ofNullable(shopId).map(REPORT.PARTY_SHOP_ID::eq).orElse(trueCondition())))
+    public Report getReportDoUpdateSkipLocked(long reportId) throws DaoException {
+        Query query = getDslContext().selectFrom(REPORT)
+                .where(REPORT.ID.eq(reportId))
                 .forUpdate()
                 .skipLocked();
         return fetchOne(query, reportRowMapper);
