bc5fd7334e0662572e7ff4a78eeb8cb5424a1d7a
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2019-10-23 20:54:33 +0300

    Fixed name

--- src/main/java/com/rbkmoney/reporter/util/TokenUtil.java
+++ src/main/java/com/rbkmoney/reporter/util/TokenUtil.java
@@ -18,8 +18,8 @@ public class TokenUtil {
     private static ObjectMapper objectMapper = new ObjectMapper();
 
     @SneakyThrows
-    public static String buildToken(ReportRequest request, List<String> reportTypes, String time) {
-        return Base64.getEncoder().encodeToString(objectMapper.writeValueAsBytes(new TokenData(request, reportTypes, time)));
+    public static String buildToken(ReportRequest request, List<String> reportTypes, String whereTime) {
+        return Base64.getEncoder().encodeToString(objectMapper.writeValueAsBytes(new TokenData(request, reportTypes, whereTime)));
     }
 
     public static boolean isValid(StatReportRequest statReportRequest) {
@@ -35,7 +35,7 @@ public class TokenUtil {
 
     @SneakyThrows
     public static String extractTime(String token) {
-        return objectMapper.readValue(Base64.getDecoder().decode(token), TokenData.class).getTime();
+        return objectMapper.readValue(Base64.getDecoder().decode(token), TokenData.class).getWhereTime();
     }
 
     @Data
@@ -47,10 +47,10 @@ public class TokenUtil {
         private String fromTime;
         private String toTime;
         private List<String> reportTypes;
-        private String time;
+        private String whereTime;
 
-        public TokenData(ReportRequest request, List<String> reportTypes, String time) {
-            this(request.getPartyId(), request.getShopId(), request.getTimeRange().getFromTime(), request.getTimeRange().getToTime(), reportTypes, time);
+        public TokenData(ReportRequest request, List<String> reportTypes, String whereTime) {
+            this(request.getPartyId(), request.getShopId(), request.getTimeRange().getFromTime(), request.getTimeRange().getToTime(), reportTypes, whereTime);
         }
     }
 }
