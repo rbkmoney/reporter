68912b47902c591ce72ecb60f85589944aa321f1
d.baykov <d.baykov@rbkmoney.com>
2020-05-23 20:39:44 +0300

    REP-66: Fix test env

--- src/test/java/com/rbkmoney/reporter/config/AbstractInvoicingServiceConfig.java
+++ src/test/java/com/rbkmoney/reporter/config/AbstractInvoicingServiceConfig.java
@@ -8,13 +8,19 @@ import com.rbkmoney.reporter.service.ReportNewProtoService;
 import com.rbkmoney.reporter.service.StorageService;
 import com.rbkmoney.reporter.service.impl.TaskServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.ClassRule;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.FailureDetectingExternalResource;
 
 import java.util.function.Supplier;
 
@@ -22,7 +28,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@ContextConfiguration(classes = ReporterApplication.class)
+@ContextConfiguration(classes = ReporterApplication.class, initializers = AbstractIntegrationConfig.Initializer.class)
 @TestPropertySource("classpath:application.yml")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Slf4j
@@ -41,6 +47,40 @@ public abstract class AbstractInvoicingServiceConfig {
             TestContainersBuilder.builderWithTestContainers(getTestContainersParametersSupplier())
                     .addPostgresqlTestContainer()
                     .build();
+
+    @ClassRule
+    public static final FailureDetectingExternalResource resource = new FailureDetectingExternalResource() {
+
+        @Override
+        protected void starting(Description description) {
+            testContainers.startTestContainers();
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            log.warn("Test Container running was failed ", e);
+        }
+
+        @Override
+        protected void finished(Description description) {
+            testContainers.stopTestContainers();
+        }
+    };
+
+    public static class Initializer extends ConfigFileApplicationContextInitializer {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            super.initialize(configurableApplicationContext);
+            TestPropertyValues.of(
+                    testContainers.getEnvironmentProperties(
+                            environmentProperties -> {
+                            }
+                    )
+            )
+                    .applyTo(configurableApplicationContext);
+        }
+    }
 
     private static Supplier<TestContainersParameters> getTestContainersParametersSupplier() {
         return () -> {
