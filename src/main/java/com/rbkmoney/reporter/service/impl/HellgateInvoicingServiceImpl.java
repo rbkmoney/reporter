e6712ec3ead5c5940fb59a9f1b744133a263b1f8
d.baykov <d.baykov@rbkmoney.com>
2020-06-16 15:57:20 +0300

    REP-66: Add throthling

---
+++ src/main/java/com/rbkmoney/reporter/service/impl/HellgateInvoicingServiceImpl.java
@@ -0,0 +1,32 @@
+package com.rbkmoney.reporter.service.impl;
+
+import com.rbkmoney.damsel.payment_processing.*;
+import com.rbkmoney.reporter.service.HellgateInvoicingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HellgateInvoicingServiceImpl implements HellgateInvoicingService {
+
+    private final InvoicingSrv.Iface hgInvoicingService;
+
+    @Value("${kafka.topics.invoicing.throttling-timeout-ms}")
+    private int throttlingTimeout;
+
+    private static final UserInfo USER_INFO = new UserInfo()
+            .setId("admin")
+            .setType(UserType.service_user(new ServiceUser()));
+
+    @Override
+    public Invoice getInvoice(String invoiceId, long sequenceId) throws Exception {
+        Thread.sleep(throttlingTimeout);
+        return hgInvoicingService.get(USER_INFO, invoiceId, getEventRange((int) sequenceId));
+    }
+
+    private EventRange getEventRange(int sequenceId) {
+        return new EventRange().setLimit(sequenceId);
+    }
+
+}
